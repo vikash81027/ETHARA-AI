cf0c95b2da4b8bd96726f25f6ec3c3afe47979a9
vikash <vikashkkumar6033@gmail.com>
2026-05-01 02:04:04 +0530

    Fix Vite CustomEvent error and package.json scripts

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Fix for CustomEvent error in Node environments during Vite build
+if (typeof global !== 'undefined') {
+  if (typeof global.Event === 'undefined') {
+    global.Event = class Event {};
+  }
+  if (typeof global.CustomEvent === 'undefined') {
+    global.CustomEvent = class CustomEvent extends global.Event {};
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
